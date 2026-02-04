a91cee41c23c3d0aa8adc8a150da76f83d1c6bca
Ezequiel Suasnabar <ezequiel.suasnabar@gmail.com>
2026-02-04 20:34:05 -0300

    Shell script to get AWS IAM Policies created for the project and all the related outputs

---
+++ terraform/AWS/get_aws_iam_policies.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+aws configure --profile practical-task
+
+for POLICY in `aws iam list-policies --output text | grep Terraform | awk -F " " '{print $2"_"$5"_"$10}'`
+do
+	PARAMS=`echo ${POLICY} | awk -F "_" '{printf "--policy-arn "$1" --version-id "$2}'`
+	OUTPUT_NAME_FILE=`echo ${POLICY} | awk -F "_" '{printf $3"_"$2}'`.txt
+	
+	echo "aws iam get-policy-version ${PARAMS} terraform/AWS/IAM_policies/${OUTPUT_NAME_FILE} \n"
+	echo -e "aws iam get-policy-version ${PARAMS}\n" > IAM_policies/${OUTPUT_NAME_FILE}
+	aws iam get-policy-version ${PARAMS} >> IAM_policies/${OUTPUT_NAME_FILE}
+	read popo
+done
+
+ls -ltr terraform/AWS/IAM_policies/
+
